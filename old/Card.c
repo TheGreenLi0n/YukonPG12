031017fbb8e4984254a01bb055eb20977797a7a0
TheGreenLi0n <s200260@student.dtu.dk>
2023-05-06 16:48:19 +0200

    Added old code for reference

---
+++ old/Card.c
@@ -0,0 +1,188 @@
+#include <stdbool.h>
+#include <malloc.h>
+#include <stdlib.h>
+
+
+typedef struct Card {
+    char value[2];
+    bool visible;
+    struct Card* next;
+} card;
+
+
+//Creates the deck of cards with the right cards of a standard card set.
+card* createDeck (char filename[]) {
+    char nameOfFile[28];
+    card* head = NULL, *temp, *tail = NULL;
+
+    FILE* f;
+    if(filename == "") {
+
+        strcpy(nameOfFile, "StandardDeck.txt");
+
+        f = fopen(nameOfFile, "w");
+        fprintf(f, "AC\n");
+        fprintf(f, "2C\n");
+        fprintf(f, "3C\n");
+        fprintf(f, "4C\n");
+        fprintf(f, "5C\n");
+        fprintf(f, "6C\n");
+        fprintf(f, "7C\n");
+        fprintf(f, "8C\n");
+        fprintf(f, "9C\n");
+        fprintf(f, "TC\n");
+        fprintf(f, "JC\n");
+        fprintf(f, "QC\n");
+        fprintf(f, "KC\n");
+        fprintf(f, "AD\n");
+        fprintf(f, "2D\n");
+        fprintf(f, "3D\n");
+        fprintf(f, "4D\n");
+        fprintf(f, "5D\n");
+        fprintf(f, "6D\n");
+        fprintf(f, "7D\n");
+        fprintf(f, "8D\n");
+        fprintf(f, "9D\n");
+        fprintf(f, "TD\n");
+        fprintf(f, "JD\n");
+        fprintf(f, "QD\n");
+        fprintf(f, "KD\n");
+        fprintf(f, "AH\n");
+        fprintf(f, "2H\n");
+        fprintf(f, "3H\n");
+        fprintf(f, "4H\n");
+        fprintf(f, "5H\n");
+        fprintf(f, "6H\n");
+        fprintf(f, "7H\n");
+        fprintf(f, "8H\n");
+        fprintf(f, "9H\n");
+        fprintf(f, "TH\n");
+        fprintf(f, "JH\n");
+        fprintf(f, "QH\n");
+        fprintf(f, "KH\n");
+        fprintf(f, "AS\n");
+        fprintf(f, "2S\n");
+        fprintf(f, "3S\n");
+        fprintf(f, "4S\n");
+        fprintf(f, "5S\n");
+        fprintf(f, "6S\n");
+        fprintf(f, "7S\n");
+        fprintf(f, "8S\n");
+        fprintf(f, "9S\n");
+        fprintf(f, "TS\n");
+        fprintf(f, "JS\n");
+        fprintf(f, "QS\n");
+        fprintf(f, "KS\n");
+
+        fclose(f);
+    }
+    else{
+        strcpy(nameOfFile, filename);
+        strcat(nameOfFile, ".txt");
+    }
+
+    f = fopen(nameOfFile, "r");
+
+    temp = (card*)malloc(sizeof(card));
+    while (fscanf(f, "%s,", temp -> value) != EOF){
+        if (head == NULL){
+            head = temp;
+        }
+        else{
+            tail -> next = temp;
+        }
+        tail = temp;
+        tail -> next = NULL;
+        temp = (card*) malloc(sizeof(card));
+    }
+    fclose(f);
+    return head;
+}
+
+//Shuffles the deck, splitting it into two piles based on the split-value.
+struct card* splitShuffle(card* head, int split){
+
+    card* head1 = NULL;
+    card* head2 = NULL;
+    card* tail1 = NULL;
+    card* newDeck = NULL;
+    card* tmp;
+    int splitRest = 52-split;
+    int shuffleNO;
+    int biggestPile;
+
+    head1 = head;
+    head2 = head;
+    tail1 = head1;
+    if(split == 52 || split == 0){
+        return head;
+    }
+
+
+    for (int i = 0; i < split; ++i) {
+        head2 = head2->next;
+    }
+
+    for (int i = 0; i < split-1; ++i){
+        tail1 = tail1->next;
+    }
+
+    if(split>splitRest){
+        shuffleNO = splitRest;
+        biggestPile = 1;
+    } else{
+        shuffleNO = split;
+        biggestPile = 2;
+    }
+
+    newDeck = head1;
+    tmp = newDeck;
+    head1 = head1->next;
+    for (int i = 0; i < shuffleNO*2-1; ++i) {
+        if(i%2 == 1){
+            tmp->next = head1;
+            tmp = tmp->next;
+            head1 = head1->next;
+        } else{
+            tmp->next = head2;
+            tmp = tmp->next;
+            head2 = head2->next;
+        }
+
+    }
+
+    if(biggestPile == 1){
+        while(head1 != tail1->next){
+            tmp->next = head1;
+            tmp = tmp->next;
+            head1 = head1->next;
+
+        }
+    } else{
+        while(head2 != NULL){
+            tmp->next = head2;
+            tmp = tmp->next;
+            head2 = head2->next;
+        }
+    }
+
+    tmp->next = NULL;
+return newDeck;
+}
+
+
+//Saves the deck to a file.
+void saveDeck(card* deck, char filename[]){
+    char nameOfFile[28];
+    FILE* f;
+    strcpy(nameOfFile,filename);
+    strcat(nameOfFile,".txt");
+    f = fopen(nameOfFile,"w");
+
+    while(deck->value!=NULL){
+        fprintf(f,deck->value);
+        fprintf(f,"\n");
+        deck = deck->next;
+    }
+    fclose(f);
+}
---
+++ old/main.c
@@ -0,0 +1,492 @@
+#include <stdio.h>
+#include <string.h>
+#include "Card.c"
+
+void printStartFormat();
+card* loadDeck(char[],char[]);
+void show(char[],card*);
+void playPhase(card*,card*,card*,card*,card*,card*,card*,card*,char*);
+void printEndLines(char*,bool);
+int getLongestList(card*,card*,card*,card*,card*,card*,card*);
+void printLines(card*,card*,card*,card*,card*,card*,card*,int);
+void printEmpty(char*,char*);
+
+
+//The main method that manages the gameflow through switch cases.
+int main() {
+
+    bool gameOngoing = true;
+    int currentPhase = 0;
+    printStartFormat();
+    card* deck = NULL;
+    card* c1;
+    card* c2;
+    card* c3;
+    card* c4;
+    card* c5;
+    card* c6;
+    card* c7;
+    card* f1;
+    card* f2;
+    card* f3;
+    card* f4;
+
+    while(gameOngoing){
+        char input[20];
+
+        fgets(input,20,stdin);
+
+        int dec = input[0] + input[1];
+        int gameMove = input[2];
+        int gameMove2 = input[5];
+        int number = ((input[3]-48)*10) + (input[4]-48);
+
+        switch (dec) {
+            case 144:
+                if(currentPhase == 0){
+                    printf("\nLoad deck\n");
+                    if (input[2] != 10) {
+                        char *fileName;
+                        fileName = strstr(input, " ") + 1;
+                        strtok(fileName, "\n");
+                        deck = loadDeck(fileName, input);
+                    }
+                    else
+                        deck = loadDeck("", input);
+                } else{
+                    //need another way to print the output to the terminal
+                    //so it's adaptive to where in the process the player is at
+                    printEmpty(input,"ERROR");
+                }
+
+                break;
+            case 90:
+                if(currentPhase == 0){
+                    printf("\nPlay\n");
+                    currentPhase = 1;
+                    playPhase(c1,c2,c3,c4,c5,c6,c7,deck,input);
+                } else{
+                    printf("\nCommand not available in the PLAY phase\n");
+                }
+
+                break;
+            case 151:
+                if(currentPhase == 0){
+                    printf("\nSave deck\n");
+
+                    if (input[2] != 10) {
+                        char *fileName;
+                        fileName = strstr(input, " ") + 1;
+                        strtok(fileName, "\n");
+                        saveDeck(deck, fileName);
+                    } else{
+                        saveDeck(deck, "cards");
+                    }
+                }else{
+                    printf("\nCommand not available in the PLAY phase\n");
+                }
+
+                break;
+            case 91:
+                if(currentPhase == 1){
+                    printf("\nQuit current game\n");
+                    currentPhase = 0;
+                }
+
+                break;
+            case 170:
+                if(currentPhase == 0){
+                    printf("\nShow\n");
+                    show(input,deck);
+                }else{
+                    printf("\nCommand not available in the PLAY phase\n");
+                }
+
+                break;
+            case 162:
+                if(currentPhase == 0){
+                    printf("Quit");
+                    gameOngoing = false;
+                }else{
+                    printf("\nCommand not available in the PLAY phase\n");
+                }
+                break;
+
+            case 156:
+                if(currentPhase == 0){
+                    if(deck == NULL){
+                        printf("Error need to load deck first");
+                    } else{
+                        printf("\nSplit shuffle\n");
+                        if(number < 52 && number >0){
+                            deck = splitShuffle(deck,number);
+                        } else{
+                            number = rand() % 52;
+                            deck = splitShuffle(deck,number);
+                        }
+                    }
+                }else{
+                    printf("\nCommand not available in the PLAY phase\n");
+                }
+                break;
+            case 10:
+                break;
+
+            default:
+                if(currentPhase == 1) {
+                    if (gameMove == 45 || gameMove2 == 45) {
+                        printf("\nGame moveCard\n");
+                        printf("C1\tC2\tC3\tC4\tC5\tC6\tC7\n");
+                        printEndLines(input,true);
+                    } else {
+
+                    }
+                }
+
+                break;
+        }
+        //test if the player has won yet
+    }
+
+    return 0;
+}
+void printStartFormat(void){
+    printf("C1\tC2\tC3\tC4\tC5\tC6\tC7\n");
+    printf("\t\t\t\t\t\t\t\t[]\tF1\n");
+    printf("\t\t\t\t\t\t\t\t[]\tF2\n");
+    printf("\t\t\t\t\t\t\t\t[]\tF3\n");
+    printf("\t\t\t\t\t\t\t\t[]\tF4\n");
+    printf("LAST command:\n");
+    printf("Message:\n");
+    printf("INPUT>");
+}
+//prints out an empty version of the default output
+void printEmpty(char* lastCommand,char* message){
+    printf("C1\tC2\tC3\tC4\tC5\tC6\tC7\n");
+    printf("\t\t\t\t\t\t\t\t[]\tF1\n");
+    printf("\t\t\t\t\t\t\t\t[]\tF2\n");
+    printf("\t\t\t\t\t\t\t\t[]\tF3\n");
+    printf("\t\t\t\t\t\t\t\t[]\tF4\n");
+    printf("LAST command: %s",lastCommand);
+    printf("Message: %s\n",message);
+    printf("INPUT>");
+}
+
+//Loads the deck.
+card* loadDeck(char filename[],char command[]){
+    //Load the deck from the Card class
+    //if the name given doesn't exist in the saved deck folder, it should return an error
+    //if no file name is given it will use the default deck
+    //if the file isn't valid then it should return an error as well
+    //otherwise it should work and return with "OK"
+    bool execute;
+    card* deck = createDeck(filename);
+    card* currentCard = deck;
+    //currentCard = createDeck(filename);
+    if(currentCard->value != NULL){
+        execute = true;
+    } else{
+        execute = false;
+    }
+    printf("C1\tC2\tC3\tC4\tC5\tC6\tC7\n\n");
+    int counter = 0;
+    int lines = 0;
+
+    while(currentCard->value!=NULL){
+        printf("[]\t");
+        counter++;
+        if(counter == 7){
+            if(lines == 0){
+                printf("\t[]\tF1");
+            } else if(lines == 2){
+                printf("\t[]\tF2");
+            } else if(lines == 4){
+                printf("\t[]\tF3");
+            } else if(lines == 6){
+                printf("\t[]\tF4");
+            }
+            printf("\n");
+            counter = 0;
+            lines++;
+        }
+        currentCard = currentCard->next;
+    }
+    printEndLines(command,execute);
+    return deck;
+}
+
+//Shows the current deck of cards in their shuffled (or unshuffled) order.
+void show(char command[],card* deck){
+    bool execute;
+
+    if(deck->value != NULL){
+        execute = true;
+    } else{
+        execute = false;
+    }
+    printf("C1\tC2\tC3\tC4\tC5\tC6\tC7\n\n");
+    int counter = 0;
+    int lines = 0;
+
+    while(deck->value!=NULL){
+        printf("%s\t",deck->value);
+        counter++;
+        if(counter == 7){
+            if(lines == 0){
+                printf("\t[]\tF1");
+            } else if(lines == 2){
+                printf("\t[]\tF2");
+            } else if(lines == 4){
+                printf("\t[]\tF3");
+            } else if(lines == 6){
+                printf("\t[]\tF4");
+            }
+            printf("\n");
+            counter = 0;
+            lines++;
+        }
+        deck = deck->next;
+    }
+    printEndLines(command,execute);
+}
+
+//Initiates the PLAY phase for the game.
+void playPhase(card* c1,card* c2,card* c3,card* c4,card* c5,card* c6, card* c7,card* deck,char* command){
+    card* tmp;
+    c1 = deck;
+    c1->visible = true;
+    c2 = c1->next;
+    c1->next = NULL;
+    tmp = c2;
+    for (int i = 0; i < 5; ++i) {
+        tmp = tmp->next;
+    }
+    c3 = tmp->next;
+    tmp->next = NULL;
+    tmp = c3;
+    for (int i = 0; i < 6; ++i) {
+        tmp = tmp->next;
+    }
+    c4 = tmp->next;
+    tmp->next = NULL;
+    tmp = c4;
+    for (int i = 0; i < 7; ++i) {
+        tmp = tmp->next;
+    }
+    c5 = tmp->next;
+    tmp->next = NULL;
+    tmp = c5;
+    for (int i = 0; i < 8; ++i) {
+        tmp = tmp->next;
+    }
+    c6 = tmp->next;
+    tmp->next = NULL;
+    tmp = c6;
+    for (int i = 0; i < 9; ++i) {
+        tmp = tmp->next;
+    }
+    c7 = tmp->next;
+    tmp->next = NULL;
+    tmp = c7;
+    for (int i = 0; i < 10; ++i) {
+        tmp = tmp->next;
+    }
+    tmp->next = NULL;
+
+    //Setup game to be ready to play, print the columns in the terminal with the right cards visible
+    printf("C1\tC2\tC3\tC4\tC5\tC6\tC7\n\n");
+    //method to print each line of the columns and foundations
+    printLines(c1,c2,c3,c4,c5,c6,c7, 11);
+
+    printEndLines(command,true);
+
+
+}
+
+//Prints the three status lines: Last command, message and input.
+void printEndLines(char* lastCommand, bool success) {
+    printf("\nLAST command: %\n", lastCommand);
+    if (success) {
+        printf("Message: OK\n");
+    } else {
+        printf("Message: ERROR\n");
+    }
+    printf("INPUT>");
+}
+
+//Method to get the longest column of cards.
+int getLongestList(card* c1,card* c2,card* c3,card* c4,card* c5,card* c6,card* c7){
+    card* tmp = c1;
+    int largest = 0;
+    int counter = 0;
+    while(tmp != NULL){
+        counter++;
+        tmp = tmp->next;
+    }
+    if(counter>largest){
+        largest = counter;
+    }
+    counter = 0;
+    tmp = c2;
+    while(tmp != NULL){
+        counter++;
+        tmp = tmp->next;
+    }
+    if(counter>largest){
+        largest = counter;
+    }
+    counter = 0;
+    tmp = c3;
+    while(tmp != NULL){
+        counter++;
+        tmp = tmp->next;
+    }
+    if(counter>largest){
+        largest = counter;
+    }
+    counter = 0;
+    tmp = c4;
+    while(tmp != NULL){
+        counter++;
+        tmp = tmp->next;
+    }
+    if(counter>largest){
+        largest = counter;
+    }
+    counter = 0;
+    tmp = c5;
+    while(tmp != NULL){
+        counter++;
+        tmp = tmp->next;
+    }
+    if(counter>largest){
+        largest = counter;
+    }
+    counter = 0;
+    tmp = c6;
+    while(tmp != NULL){
+        counter++;
+        tmp = tmp->next;
+    }
+    if(counter>largest){
+        largest = counter;
+    }
+    counter = 0;
+    tmp = c7;
+    while(tmp != NULL){
+        counter++;
+        tmp = tmp->next;
+    }
+    if(counter>largest){
+        largest = counter;
+    }
+    return largest;
+}
+
+
+//Prints out the columns to the terminal, doesn't take foundations into account.
+void printLines(card* c1,card* c2,card* c3,card* c4,card* c5,card* c6, card* c7,int longest){
+    card* tmp1 = c1;
+    card* tmp2 = c2;
+    card* tmp3 = c3;
+    card* tmp4 = c4;
+    card* tmp5 = c5;
+    card* tmp6 = c6;
+    card* tmp7 = c7;
+    int num = 1;
+
+    for (int i = 0; i < longest; ++i) {
+        char c11,c12,c21,c22,c31,c32,c41,c42,c51,c52,c61,c62,c71,c72;
+        if(tmp1 != NULL){
+            c11 = tmp1->value[0];
+            c12 = tmp1->value[1];
+        } else{
+            c11 = ' ';
+            c12 = ' ';
+        }
+        if(tmp2 != NULL){
+            c21 = tmp2->value[0];
+            c22 = tmp2->value[1];
+        } else{
+            c21 = ' ';
+            c22 = ' ';
+        }
+        if(tmp3 != NULL){
+            c31 = tmp3->value[0];
+            c32 = tmp3->value[1];
+        } else{
+            c31 = ' ';
+            c32 = ' ';
+        }
+        if(tmp4 != NULL){
+            c41 = tmp4->value[0];
+            c42 = tmp4->value[1];
+        } else{
+            c41 = ' ';
+            c42 = ' ';
+        }
+        if(tmp5 != NULL){
+            c51 = tmp5->value[0];
+            c52 = tmp5->value[1];
+        } else{
+            c51 = ' ';
+            c52 = ' ';
+        }
+        if(tmp6 != NULL){
+            c61 = tmp6->value[0];
+            c62 = tmp6->value[1];
+        } else{
+            c61 = ' ';
+            c62 = ' ';
+        }
+        if(tmp7 != NULL){
+            c71 = tmp7->value[0];
+            c72 = tmp7->value[1];
+        } else{
+            c71 = ' ';
+            c72 = ' ';
+        }
+        if(i == 0||i == 2||i == 4||i == 6){
+            printf("%c%c\t%c%c\t%c%c\t%c%c\t%c%c\t%c%c\t%c%c\t\t%s\tF%i\n",c11, c12, c21,c22,c31,c32,c41,c42,c51,c52,c61,c62,c71,c72,"[]",num);
+            num++;
+        } else{
+            printf("%c%c\t%c%c\t%c%c\t%c%c\t%c%c\t%c%c\t%c%c\t\n",c11, c12, c21,c22,c31,c32,c41,c42,c51,c52,c61,c62,c71,c72);
+
+        }
+        if(tmp1 == NULL){
+            tmp1 = NULL;
+        } else {
+            tmp1 = tmp1->next;
+        }
+        if(tmp2 == NULL){
+            tmp2 = NULL;
+        } else {
+            tmp2 = tmp2->next;
+        }
+        if(tmp3 == NULL){
+            tmp3 = NULL;
+        } else {
+            tmp3 = tmp3->next;
+        }
+        if(tmp4 == NULL){
+            tmp4 = NULL;
+        } else {
+            tmp4 = tmp4->next;
+        }
+        if(tmp5 == NULL){
+            tmp5 = NULL;
+        } else {
+            tmp5 = tmp5->next;
+        }
+        if(tmp6 == NULL){
+            tmp6 = NULL;
+        } else {
+            tmp6 = tmp6->next;
+        }
+        if(tmp7 == NULL){
+            tmp7 = NULL;
+        } else {
+            tmp7 = tmp7->next;
+        }
+    }
+}
